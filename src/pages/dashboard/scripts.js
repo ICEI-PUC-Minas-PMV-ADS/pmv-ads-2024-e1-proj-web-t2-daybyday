27969be9800278407066ba8b5b7a422546292d91
FellipeMTX <fellipefacdir@gmail.com>
2024-05-05 23:45:17 -0300

    feat: make the user be able to edit and detele entries

--- src/pages/dashboard/scripts.js
+++ src/pages/dashboard/scripts.js
@@ -1,4 +1,6 @@
+
 let transactions = [];
+let editedIndex = -1;
 
 function togglePopup() {
     const popup = document.getElementById("popup");
@@ -17,22 +19,50 @@ function addTransaction() {
         return;
     }
     
-    transactions.push({ name, value });
+    if (editedIndex === -1) {
+        transactions.push({ name, value });
+    } else {
+        transactions[editedIndex] = { name, value };
+        editedIndex = -1;
+    }
+    
     displayTransactions();
     calculateTotalValue();
     nameInput.value = "";
     valueInput.value = "";
 }
 
+function editTransaction(index) {
+    const transaction = transactions[index];
+    const nameInput = document.getElementById("transactionName");
+    const valueInput = document.getElementById("transactionValue");
+    
+    nameInput.value = transaction.name;
+    valueInput.value = transaction.value;
+    editedIndex = index;
+    
+    togglePopup();
+}
+
+function deleteTransaction(index) {
+    transactions.splice(index, 1);
+    displayTransactions();
+    calculateTotalValue();
+}
+
 function displayTransactions() {
     const transactionList = document.getElementById("transactionList");
     transactionList.innerHTML = "";
     
-    transactions.forEach(transaction => {
+    transactions.forEach((transaction, index) => {
         const row = document.createElement("tr");
         row.innerHTML = `
             <td>${transaction.name}</td>
             <td>${transaction.value}</td>
+            <td>
+                <button onclick="editTransaction(${index})">Edit</button>
+                <button onclick="deleteTransaction(${index})">Delete</button>
+            </td>
         `;
         transactionList.appendChild(row);
     });
