6ef7d51b8434943f33addf3a845ee5b8a86712a2
raynanfernandes <raynanfernandes15@yahoo.com.br>
2024-06-03 17:40:59 -0300

    button 'show the password' created

--- src/pages/register/registerScript.js
+++ src/pages/register/registerScript.js
@@ -4,6 +4,7 @@ document.getElementById('registerForm').addEventListener('submit', function(even
     var password = document.getElementById('registerPassword').value;
     var confirm_password = document.getElementById("confirm_password").value;
 
+
     if (password !== confirm_password) {
         alert("Passwords do not match. Please try again.");
         return;
@@ -17,3 +18,16 @@ document.getElementById('registerForm').addEventListener('submit', function(even
 
     window.location.href = '../login/login.html';
 });
+
+document.getElementById("mostrarSenha").addEventListener("click", function(event) {
+    event.preventDefault(); // Evita o envio do formulário
+    var senhaInput = document.getElementById("registerPassword");
+    if (senhaInput.type === "password") {
+        senhaInput.type = "text";
+        document.getElementById("mostrarSenha").textContent = "Esconder Senha";
+    } else {
+        senhaInput.type = "password";
+        document.getElementById("mostrarSenha").textContent = "Mostrar Senha";
+    }
+});
+
