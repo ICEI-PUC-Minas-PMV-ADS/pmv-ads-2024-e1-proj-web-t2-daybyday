a7cbc35d5f2ce2ffb9cf6ad91882b75774e9595f
FellipeMTX <fellipefacdir@gmail.com>
2024-06-09 16:16:10 -0300

    Make error-message on sign up

--- src/pages/register/registerScript.js
+++ src/pages/register/registerScript.js
@@ -2,32 +2,29 @@ document.getElementById('registerForm').addEventListener('submit', function(even
     event.preventDefault();
     var username = document.getElementById('registerUsername').value;
     var password = document.getElementById('registerPassword').value;
-    console.log(password);
     var confirm_password = document.getElementById("confirm_password").value;
+    var errorMessage = document.getElementById("errorMessage");
 
-          if (password !== confirm_password ) {
-        alert("As senhas não coincidem! Por favor, digite a mesma senha nos dois campos.");
+    errorMessage.textContent = '';
+
+    if (password !== confirm_password) {
+        errorMessage.textContent = "As senhas não coincidem! Por favor, digite a mesma senha nos dois campos.";
         return;
     }
 
     function validPassword(password) {
-    const regex = /^(?=.*[A-Z])(?=.*[!@#$%^&*/])(?=.{8,})/;
-    return regex.test(password);
-}
-      if (validPassword(password)) {
-          alert("Senha válida!");
-      } else {
-          alert("Senha inválida! A senha deve conter pelo menos 8 caracteres, uma letra maiúscula e um caractere especial.");
+        const regex = /^(?=.*[A-Z])(?=.*[!@#$%^&*/])(?=.{8,})/;
+        return regex.test(password);
+    }
+
+    if (!validPassword(password)) {
+        errorMessage.textContent = "Senha inválida! A senha deve conter pelo menos 8 caracteres, uma letra maiúscula e um caractere especial.";
         return;
-        }
-    
+    }
 
     localStorage.setItem('username', username);
     localStorage.setItem('password', password);
-    
 
     alert("Cadastro realizado com sucesso! Faça login para acessar a plataforma.");
-    console.log('Login:', username, password);
-
     window.location.href = '../login/login.html';
 });
