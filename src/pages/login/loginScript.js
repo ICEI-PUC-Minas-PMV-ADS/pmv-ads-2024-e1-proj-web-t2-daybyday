6cfd7bc5ab421701f65ad997a5460abb0ca01ecd
Bruno Monteiro <bml0401@gmail.com>
2024-06-09 13:29:15 -0300

    Resolução problema id usuário

--- src/pages/login/loginScript.js
+++ src/pages/login/loginScript.js
@@ -1,37 +1,40 @@
 document
-.getElementById("loginForm")
-.addEventListener("submit", function (event) {
-  event.preventDefault();
-  var emailOrUsername = document.getElementById("loginInput").value;
-  var password = document.getElementById("loginPassword").value;
-  var storedUsername = localStorage.getItem("username");
-  var storedEmail = localStorage.getItem("email");
-  var storedPassword = localStorage.getItem("password");
-  
-  if (emailOrUsername.includes("@")) {
-    var email = emailOrUsername.toLowerCase();
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    var emailOrUsername = document.getElementById("loginUsername").value;
+    var password = document.getElementById("loginPassword").value;
+    var storedUsername = localStorage.getItem("username");
+    var storedEmail = localStorage.getItem("email");
+    var storedPassword = localStorage.getItem("password");
+
+    if (emailOrUsername.includes("@")) {
+      var email = emailOrUsername.toLowerCase();
       if (!validateEmail(email)) {
-          alert("Email inválido");
-          return;
+        alert("Email inválido");
+        return;
       }
-  } else {
+    } else {
       var username = emailOrUsername;
-  }
-  
-  if ((username && username === storedUsername) || (email && email === storedEmail)) {
+    }
+
+    if (
+      (username && username === storedUsername) ||
+      (email && email === storedEmail)
+    ) {
       if (password === storedPassword) {
-          alert("Login successful!");
-          console.log("Login successful");
-          window.location.href = "../dashboard/dashboard.html";
+        alert("Login successful!");
+        console.log("Login successful");
+        window.location.href = "../dashboard/dashboard.html";
       } else {
-          alert("Senha incorreta");
-          console.log("Senha incorreta");
+        alert("Senha incorreta");
+        console.log("Senha incorreta");
       }
-  } else {
+    } else {
       alert("Usuário ou email não encontrado");
       console.log("Usuário ou email não encontrado");
-  }
-});
+    }
+  });
 
 function validateEmail(email) {
   var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
