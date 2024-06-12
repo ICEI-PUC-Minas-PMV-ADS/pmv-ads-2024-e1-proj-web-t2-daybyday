c7698471da580f8f2ffc8349ae59289e98b40aac
Toledodev <rafael.toledo@engenharia.ufjf.br>
2024-06-12 15:54:27 -0300

    refactor

--- src/pages/register/registerScript.js
+++ src/pages/register/registerScript.js
@@ -1,46 +1,51 @@
-document.getElementById('registerForm').addEventListener('submit', function(event) {
+document
+  .getElementById("registerForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    var username = document.getElementById('registerUsername').value;
-    var password = document.getElementById('registerPassword').value;
+    var username = document.getElementById("registerUsername").value;
+    var password = document.getElementById("registerPassword").value;
     var confirm_password = document.getElementById("confirm_password").value;
     var errorMessage = document.getElementById("errorMessage");
 
-    errorMessage.textContent = '';
+    errorMessage.textContent = "";
 
     if (password !== confirm_password) {
-        errorMessage.textContent = "As senhas não coincidem! Por favor, digite a mesma senha nos dois campos.";
-        return;
+      errorMessage.textContent =
+        "As senhas não coincidem! Por favor, digite a mesma senha nos dois campos.";
+      return;
     }
 
     function validPassword(password, username) {
-        const minLength = /^(?=.{8,})/;
-        const upperCase = /^(?=.*[A-Z])/;
-        const specialChar = /^(?=.*[!@#$%^&*/])/;
+      const minLength = /^(?=.{8,})/;
+      const upperCase = /^(?=.*[A-Z])/;
+      const specialChar = /^(?=.*[!@#$%^&*/])/;
 
-        if (!minLength.test(password)) {
-            return "A senha deve conter pelo menos 8 caracteres.";
-        }
-        if (!upperCase.test(password)) {
-            return "A senha deve conter pelo menos uma letra maiúscula.";
-        }
-        if (!specialChar.test(password)) {
-            return "A senha deve conter pelo menos um caractere especial.";
-        }
-        if (password === username) {
-            return "A senha não pode ser igual ao nome de usuário.";
-        }
-        return '';
+      if (!minLength.test(password)) {
+        return "A senha deve conter pelo menos 8 caracteres.";
+      }
+      if (!upperCase.test(password)) {
+        return "A senha deve conter pelo menos uma letra maiúscula.";
+      }
+      if (!specialChar.test(password)) {
+        return "A senha deve conter pelo menos um caractere especial.";
+      }
+      if (password === username) {
+        return "A senha não pode ser igual ao nome de usuário.";
+      }
+      return "";
     }
 
     var passwordError = validPassword(password, username);
     if (passwordError) {
-        errorMessage.textContent = passwordError;
-        return;
+      errorMessage.textContent = passwordError;
+      return;
     }
 
-    localStorage.setItem('username', username);
-    localStorage.setItem('password', password);
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
 
-    alert("Cadastro realizado com sucesso! Faça login para acessar a plataforma.");
-    window.location.href = '../login/login.html';
-});
+    alert(
+      "Cadastro realizado com sucesso! Faça login para acessar a plataforma."
+    );
+    window.location.href = "../login/login.html";
+  });
--- src/script.js
+++ src/script.js
@@ -1,32 +1,34 @@
 let activeCarouselItemIndex = 0;
-const carouselItems = document.querySelectorAll('.carousel-item');
-const carouselIndicators = document.querySelectorAll('.carousel-indicator');
-const leftArrow = document.querySelector('.carousel-arrow-left');
-const rightArrow = document.querySelector('.carousel-arrow-right');
+const carouselItems = document.querySelectorAll(".carousel-item");
+const carouselIndicators = document.querySelectorAll(".carousel-indicator");
+const leftArrow = document.querySelector(".carousel-arrow-left");
+const rightArrow = document.querySelector(".carousel-arrow-right");
 
 function updateIndicators(activeIndex) {
-    carouselIndicators.forEach((indicator) => {
-      indicator.classList.remove('active');
-    });
-  
-    carouselIndicators[activeIndex].classList.add('active');
-  }
+  carouselIndicators.forEach((indicator) => {
+    indicator.classList.remove("active");
+  });
+
+  carouselIndicators[activeIndex].classList.add("active");
+}
 
 function changeCarouselItem(newIndex) {
-  carouselItems[activeCarouselItemIndex].classList.remove('active');
+  carouselItems[activeCarouselItemIndex].classList.remove("active");
 
   activeCarouselItemIndex = newIndex;
-  carouselItems[activeCarouselItemIndex].classList.add('active');
+  carouselItems[activeCarouselItemIndex].classList.add("active");
 
   updateIndicators(activeCarouselItemIndex);
 }
 
-leftArrow.addEventListener('click', (event) => {
+leftArrow.addEventListener("click", (event) => {
   event.preventDefault();
-  changeCarouselItem((activeCarouselItemIndex - 1 + carouselItems.length) % carouselItems.length);
+  changeCarouselItem(
+    (activeCarouselItemIndex - 1 + carouselItems.length) % carouselItems.length
+  );
 });
 
-rightArrow.addEventListener('click', (event) => {
+rightArrow.addEventListener("click", (event) => {
   event.preventDefault();
   changeCarouselItem((activeCarouselItemIndex + 1) % carouselItems.length);
 });
