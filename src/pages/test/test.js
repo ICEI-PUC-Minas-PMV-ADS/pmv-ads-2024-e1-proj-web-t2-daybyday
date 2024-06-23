8f7b6cc19cb5d3d5d1e32d0af2f4b3209d5665c4
FellipeMTX <fellipefacdir@gmail.com>
2024-06-23 14:47:48 -0300

    arruma muita coisa na tabela, no css, nas conntas, coloca descricao no js e faz mais um monte de pequenos detalhes

--- src/pages/test/test.js
+++ src/pages/test/test.js
@@ -10,15 +10,20 @@ function deleteTransaction(index) {
 }
 
 function displayTransactions() {
+  // define quais serao as transacoes mostradas
   const transactionsToShow = transactions;
+  // acha o elemento da tabela
   const transactionList = document.getElementById("transactionList");
   transactionList.innerHTML = "";
 
   transactionsToShow.forEach((transaction, index) => {
+    // cria a linha da tabela
     const row = document.createElement("tr");
+    // define o nome da transacao
     const displayName = transaction.isMonthly
       ? `Mensal - ${transaction.name}`
       : transaction.name;
+    // define o codigo da tabela - linha com o nome, valor e tags, além dos botoes de editar e deletar
     row.innerHTML = `
             <td class="tableText-name">
                 ${displayName}
@@ -28,29 +33,57 @@ function displayTransactions() {
                 </div>
             </td>
             <td class="tableText">${transaction.value}</td>
-            <td class="tableText">${transaction.tags.join(", ")}</td>
+            <td class="tableText">${transaction.tags}</td>
         `;
+    // adiciona a linha como filha do elemento pricipal na tabela
     transactionList.appendChild(row);
   });
 }
 
-function calculateTotalValue(transactionsToCalculate = transactions) {
-  const totalValueElement = document.getElementById("totalValue");
-  const negativeTotalElement = document.getElementById("negativeTotal");
-  const positiveTotalElement = document.getElementById("positiveTotal");
+function calculateTotalValue(transacoes = transactions) {
+  // define os elementos que serao alterados
+  const saldoAtual = document.getElementById("saldoAtual");
+  const despesas = document.getElementById("desepesas");
+  const receitas = document.getElementById("receitas");
   const monthlyGoal = parseFloat(localStorage.getItem("monthlyBudget"));
-
-  let total = 0;
-
-  transactionsToCalculate.forEach((transaction) => {
-    total += transaction.value;
+  
+  let positivos = [];
+  let negativos = [];
+  let saldoSum = 0;
+  let despesasSum = 0;
+  let receitasSum = 0;
+  // usa o forEach para separar as transacoes em positivas e negativas
+  transacoes.forEach((transaction) => {
+    if (transaction.value >= 0) {
+      positivos.push(transaction.value);
+    } else {
+      negativos.push(transaction.value);
+    }
   });
 
-  let positiveTotal = total < monthlyGoal ? monthlyGoal - total : 0;
-  let negativeTotal = total > monthlyGoal ? total - monthlyGoal : 0;
-  totalValueElement.textContent = total.toFixed(2);
-  negativeTotalElement.textContent = negativeTotal.toFixed(2);
-  positiveTotalElement.textContent = positiveTotal.toFixed(2);
+
+  // soma o valor de todas as transacoes
+  transacoes.forEach((transaction) => {
+    saldoSum += transaction.value;
+  });
+  // soma o valor das despesas
+  negativos.forEach((negative) => {
+    despesasSum += negative;
+  });
+  // soma o valor das receitas
+  positivos.forEach((positive) => {
+    receitasSum += positive;
+  });
+
+  // console de todos os valores
+  console.log(positivos, negativos, saldoSum, despesasSum, receitasSum);
+
+  
+  // let positiveTotal = saldoSum < monthlyGoal ? monthlyGoal - saldoSum : 0;
+  // let negativeTotal = saldoSum > monthlyGoal ? saldoSum - monthlyGoal : 0;
+  saldoAtual.textContent = `R$${saldoSum.toFixed(2)}`;
+  despesas.textContent = `R$${despesasSum.toFixed(2)}`;
+  receitas.textContent = `R$${receitasSum.toFixed(2)}`;
 }
 
 function filterTransactions() {
@@ -103,6 +136,7 @@ updateFilterOptions();
 document
   .getElementById("logout-btn")
   .addEventListener("click", function (event) {
+    console.log('rodei')
     event.preventDefault();
     const confirmation = confirm("Tem certeza que deseja desconectar?");
     if (confirmation) {
@@ -182,20 +216,28 @@ function playClickSound() {
 }
 
 function addTransaction() {
+  // acha os inputs da transacao nome, valor e tags
   const nameInput = document.getElementById("transactionName");
   const valueInput = document.getElementById("transactionValue");
   const tagsInput = document.getElementById("transactionTags");
 
+  // define const com os valores dos inputs
   const name = nameInput.value;
+  // como recebemos a string do input, precisamos transformar em float
   const value = parseFloat(valueInput.value);
-  const tags = tagsInput.value.split(",").map((tag) => tag.trim());
+  const tags = tagsInput.value;
+  console.log(name, value, tags, tagsInput.value);
 
+  // se o nome nao for preenchido ou o valor nao for um numero, alerta
   if (!name || isNaN(value)) {
     alert("Por favor, preencha todos os campos corretamente.");
     return;
   }
 
+  // verifica se é uma conta mensal
   var isMonthlyBill = document.getElementById("monthlyBill").checked;
+
+  // cria um objeto com os valores da transacao
   var newTransaction = {
     name: name,
     value: value,
@@ -203,14 +245,18 @@ function addTransaction() {
     isMonthly: isMonthlyBill,
   };
 
+  // se o index de edicao for -1, adiciona a transacao, se nao, edita a transacao
   if (editedIndex === -1) {
     transactions.push(newTransaction);
   } else {
     transactions[editedIndex] = newTransaction;
     editedIndex = -1;
   }
+
+  // joga o array de transacoes pro local storage
   localStorage.setItem("transactions", JSON.stringify(transactions));
 
+  // roda a funcao do que deve ser mostrado na tabela
   displayTransactions();
   calculateTotalValue();
 
@@ -265,4 +311,13 @@ function setMonthlyBudget() {
   alert("Meta mensal estipulada para " + monthlyBudget);
   // fecha o popup
   toggleBudgetPopup();
+}
+
+// logout
+
+function handleLogout() {
+  const confirmation = confirm("Tem certeza que deseja desconectar?");
+  if (confirmation) {
+    window.location.href = "/src/pages/login/login.html";
+  }
 }
